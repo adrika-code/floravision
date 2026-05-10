1109ab5849577719131f296de11adc663e07e57b
adrika <srivastavaadria36@gmail.com>
2026-05-10 17:27:57 +0530

    add trending plants section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '@/components/Navbar'
 import HeroBanner from '@/components/HeroBanner'
+import TrendingPlants from '@/components/TrendingPlants'
 export default function Home(){
   return(
     <main>
       <Navbar/>
       <HeroBanner/>
+      <TrendingPlants/>
     </main>
   )
 }
