d27208ab1a76dbf3f6366ac9c2a393a5b6bf8dfa
Your Name <your-email@gmail.com>
2026-05-11 09:40:47 +0000

    fix o2 plants mobile responsive

--- src/components/plantss.jsx
+++ src/components/plantss.jsx
@@ -1,28 +1,31 @@
 import Image from 'next/image'
+
 export default function Plantss(){
     return(
-        <section className="px-10 py-20">
-            <div className="flex items-center justify-between bg-white/5 rounded-[60px] border border-white/10 p-10">
-            <div className="relative w-[400px] h-[400px]">
-                <Image
-                src="/images/aglaonema.png"
-                alt="o2 plant"
-                fill 
-                className="object-contain"
-                />
-
-            </div>
-            <div className="flex flex-col gap-6 max-w-lg">
-                <h2 className="text-white/75 text-4xl font-semibold">
-                We Have Small And Best O2 Plants Collection's</h2>
-                <p className="text-white/75 text-xl">
-                Oxygen producing plants, often referred as "O2 plants,"
-                are those that release oxygen into the atmosphere through the process of photosynthesis.</p>
-                <button className="w-fit px-8 py-3 bg-white/75 text-black rounded-xl text-xl">Explore</button>
-        
-            </div>
+        <section className="px-6 md:px-10 py-20">
+            <div className="flex flex-col md:flex-row items-center justify-between bg-white/5 rounded-[60px] border border-white/10 p-10 gap-10">
+                <div className="relative w-[300px] h-[300px] md:w-[400px] md:h-[400px]">
+                    <Image
+                    src="/images/aglaonema.png"
+                    alt="o2 plant"
+                    fill
+                    className="object-contain"
+                    />
+                </div>
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2 className="text-white/75 text-3xl md:text-4xl font-semibold">
+                        We Have Small And Best O2 Plants Collection's
+                    </h2>
+                    <p className="text-white/75 text-xl">
+                        Oxygen producing plants, often referred as "O2 plants,"
+                        are those that release oxygen into the atmosphere through 
+                        the process of photosynthesis.
+                    </p>
+                    <button className="w-fit px-8 py-3 bg-white/75 text-black rounded-xl text-xl">
+                        Explore
+                    </button>
+                </div>
             </div>
         </section>
     )
-    
 }
